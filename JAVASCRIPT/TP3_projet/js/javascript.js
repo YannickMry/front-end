3c08d8b91a30dd276967c6cc4a4fcdd1c0374147
Yannick Maury <yannick.maury@iepg.fr>
2019-11-22 11:47:30 +0100

    partie3 started

---
+++ JAVASCRIPT/TP3_projet/js/javascript.js
@@ -0,0 +1,32 @@
+var destinations = {
+    madagascar: {
+        image: 'img/madagascar.jpg',
+        alt: 'image madagascar',
+        offre: 'Circuit aquatique, Lorem ipsum dolor sit amet',
+        prix: '700€',
+        details: 'Madagascar, Circuit aquatique, Lorem ipsum dolor sit amet, 700€',
+    },
+    caraibe: {
+        image: 'img/maison.jpg',
+        alt: 'image maison',
+        offre: 'Circuit exploration sous marine, Lorem ipsum dolor sit amet',
+        prix: '900€',
+        details: 'Caraïbe, Circuit exploration sous marine, Lorem ipsum dolor sit amet, 900€',
+    },
+    canada: {
+        image: 'img/montagne.jpg',
+        alt: 'image montagnes',
+        offre: 'Circuit montagne, Lorem ipsum dolor sit amet',
+        prix: '999€',
+        details: 'Canada, Circuit montagne, Lorem ipsum dolor sit amet, 999€',
+    },
+    espagne: {
+        image: 'img/ocean.jpg',
+        alt: 'image ocean',
+        offre: 'Circuit océan, Lorem ipsum dolor sit amet',
+        prix: '1050€',
+        details: 'Espagne, Circuit océan, Lorem ipsum dolor sit amet, 1050',
+    },
+}
+
+console.log(destinations)
